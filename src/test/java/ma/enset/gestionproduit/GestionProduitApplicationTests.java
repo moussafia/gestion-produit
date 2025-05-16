041be811b3f409924b4c1dce334882026d16dd60
moussafia <moussafia.mohammed1999@gmail.com>
2025-05-16 21:43:32 +0100

    install spring boot project

---
+++ src/test/java/ma/enset/gestionproduit/GestionProduitApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.gestionproduit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionProduitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
