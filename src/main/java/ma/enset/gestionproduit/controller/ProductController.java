6bb2bd8086784715772c76a0101fedce8352e787
moussafia <moussafia.mohammed1999@gmail.com>
2025-05-25 19:52:05 +0100

    delete product

--- src/main/java/ma/enset/gestionproduit/controller/ProductController.java
+++ src/main/java/ma/enset/gestionproduit/controller/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -18,5 +19,10 @@ public class ProductController {
         List<Product>  products = productRepository.findAll();
         model.addAttribute("productList", products);
         return "products";
+    }
+    @GetMapping("/delete")
+    public String delete(@RequestParam(name = "id") Long id) {
+        productRepository.deleteById(id);
+        return "redirect:/index";
     }
 }
