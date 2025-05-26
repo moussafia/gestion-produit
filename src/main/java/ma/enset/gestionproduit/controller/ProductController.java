a11fb9857c31bf23d7beca735d62773253d48fcc
moussafia <moussafia.mohammed1999@gmail.com>
2025-05-26 22:10:16 +0100

    add page template thymleaf layout and add new product

--- src/main/java/ma/enset/gestionproduit/controller/ProductController.java
+++ src/main/java/ma/enset/gestionproduit/controller/ProductController.java
@@ -1,11 +1,14 @@
 package ma.enset.gestionproduit.controller;
 
+import jakarta.validation.Valid;
 import ma.enset.gestionproduit.entities.Product;
 import ma.enset.gestionproduit.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -20,9 +23,27 @@ public class ProductController {
         model.addAttribute("productList", products);
         return "products";
     }
+
+    @GetMapping("")
+    public String home() {
+        return "redirect:/index";
+    }
     @GetMapping("/delete")
     public String delete(@RequestParam(name = "id") Long id) {
         productRepository.deleteById(id);
+        return "redirect:/index";
+    }
+
+    @GetMapping("/newProduct")
+    public String newProduct(Model model) {
+        model.addAttribute("product", new Product());
+        return "newProduct";
+    }
+
+    @PostMapping("/saveProduct")
+    public String saveProduct(@Valid Product product, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) return "newProduct";
+        productRepository.save(product);
         return "redirect:/index";
     }
 }
